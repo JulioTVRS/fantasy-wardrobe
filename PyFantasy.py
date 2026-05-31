138e1cc25f23696f601a742b4200bceab5022520
JulioTVRS <juliotavrs@gmail.com>
2026-05-31 13:38:17 -0300

    Add: Submódulo de adição de produtos

--- PyFantasy.py
+++ PyFantasy.py
@@ -1,9 +1,10 @@
 roupas = {
     "Comuns": {                
-        "Vestido": {
-            "Estoque": 15,
-            "Preco": 80
-            # ID, DESCRICAO, TAMANHO, VALOR
+        1: {
+            "Nome": "Vestido",
+            "Valor": 80,
+            "Descricao": "Vestido vermelho com listras.",
+            "Tamanho": "G"
         },
     },
     "Fantasias": {}
@@ -44,30 +45,66 @@ while resp != "0":
             resp_roupas = input("Digite o número do submódulo que quer acessar: ")
 
             if resp_roupas == "1":
+                print()
                 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
                 print("|-                                -|")
                 print("|-        Todas as Roupas         -|")
                 print("|-           e Fantasias          -|")
                 print("|-                                -|")
                 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-                
                 print()
+                
                 for key, value in roupas["Comuns"].items():
-                    print(">", key, "- Estoque:", value["Estoque"])
+                    print("> ID:", key,"-", value["Nome"], "- Tamanho:", value["Tamanho"], "- Valor: R$", value["Valor"])
+                    
                 print()
                 input("Aperte (ENTER) para continuar.")
                 print()
 
             elif resp_roupas == "2":
+                print()
                 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
                 print("|-                            -|")
-                print("|-  SubMódulo de ADD produtos -|")
+                print("|-      Adicionar produto     -|")
                 print("|-                            -|")
                 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-                print("|-                            -|")
-                print("|-     Em desenvolvimento     -|")
-                print("|-                            -|")
-                print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+                print()
+                
+                nome_produto = input("Digite o nome do produto: ")
+                
+                while True:
+                    try:
+                        valor_produto = float(input("Digite o valor do produto: "))
+                    except ValueError:
+                        print()
+                        print("Erro: Digite um número válido.")
+                        continue
+
+                    if valor_produto < 0:
+                        print()
+                        print("Erro: O valor do produto não pode ser negativo.")
+                        continue
+
+                    break
+                
+                desc_produto = input("Digite a descrição do produto: ")
+                print("Padrão de tamanhos: (PP, P, M, G, GG, XG)")
+                tam_produto = input("Digite o tamanho do produto: ")
+                
+                while tam_produto.lower() not in ["pp", "p", "m", "g", "gg", "xg"]:
+                    print()
+                    print("Tamanho inválido. Tente novamente! Tamanhos válidos: (PP, P, M, G, GG, XG)")
+                    tam_produto = input("Digite o tamanho do produto: ")
+                    
+                id_produto = len(roupas["Comuns"]) + 1
+                roupas["Comuns"][id_produto] = {
+                    "Nome": nome_produto,
+                    "Valor": valor_produto,
+                    "Descricao": desc_produto,
+                    "Tamanho": tam_produto
+                }
+                print("(ID: %d) Produto adicionado com sucesso!"%(id_produto))
+
                 print()
                 input("Aperte (ENTER) para continuar.")
                 print()
